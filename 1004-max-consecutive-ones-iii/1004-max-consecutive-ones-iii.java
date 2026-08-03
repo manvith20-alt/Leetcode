7d8e65e44caa4188518ce30ca858ec1c162c920f
manvith20-alt <manvithnayak20@gmail.com>
2026-08-03 21:31:28 +0530

    Time: 3 ms (95.94%), Space: 52.3 MB (36.46%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,22 +3,21 @@ class Solution {
 
         int left = 0;
         int zeros = 0;
+        int right =0;
         int max = 0;
 
-        for (int right = 0; right < nums.length; right++) {
+        for (right = 0; right < nums.length; right++) {
             if (nums[right] == 0) {
                 zeros++;
             }
 
-            while (zeros > k) {
-                if (nums[left] == 0) {
+            if(zeros>k){
+                if(nums[left]==0)
                     zeros--;
-                }
                 left++;
             }
-            max = Math.max(max, right - left + 1);
         }
 
-        return max;
+        return right-left;
     }
 }
