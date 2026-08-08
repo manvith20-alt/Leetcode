58537bf901af4360b647dc8e4a17c502a1d03d65
manvith20-alt <manvithnayak20@gmail.com>
2026-08-08 23:47:12 +0530

    Time: 52 ms (20.45%), Space: 50.1 MB (69.28%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,35 +1,29 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k) {
+        int l = 0;
+        int count = 0;
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        int left = 0;
-        int count = 0;
-        int prefix = 0;
+        for (int r = 0; r < nums.length; r++) {
 
-        for (int right = 0; right < nums.length; right++) {
-
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
 
             while (map.size() > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
+                map.put(nums[l], map.get(nums[l]) - 1);
 
-                if (map.get(nums[left]) == 0) {
-                    map.remove(nums[left]);
+                if (map.get(nums[l]) == 0) {
+                    map.remove(nums[l]);
                 }
 
-                left++;
-                prefix = 0;
-            }
-            while (map.get(nums[left]) > 1) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                left++;
-                prefix++;
+                l++;
             }
 
-            if (map.size() == k) {
-                count += prefix + 1;
-            }
+            count += r - l + 1;
         }
 
         return count;
