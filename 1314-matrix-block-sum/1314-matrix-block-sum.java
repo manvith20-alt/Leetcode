494681f4abfff79310581f710e03e2bd22ca98db
manvith20-alt <manvithnayak20@gmail.com>
2026-08-12 12:44:31 +0530

    Time: 625 ms (5.71%), Space: 46.7 MB (61.51%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -1,45 +1,23 @@
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
-        int m = mat.length;
+        int m= mat.length;
         int n = mat[0].length;
+        int[][] ans = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
 
-        int[][] prefix = new int[m + 1][n + 1];
+                for(int g=i-k;g<=i+k;g++){
+                    for(int h=j-k;h<=j+k;h++){
+                        if(g >= 0 && g < m && h >= 0 && h < n){
+                            ans[i][j] += mat[g][h]; 
+                        
+                        }
 
-        // Build prefix sum
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                prefix[i][j] = mat[i - 1][j - 1]
-                        + prefix[i - 1][j]
-                        + prefix[i][j - 1]
-                        - prefix[i - 1][j - 1];
+                    }
+                }
             }
         }
 
-        int[][] answer = new int[m][n];
-
-        // Calculate block sum for every cell
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                int top = Math.max(0, i - k);
-                int bottom = Math.min(m - 1, i + k);
-
-                int left = Math.max(0, j - k);
-                int right = Math.min(n - 1, j + k);
-
-                // Convert mat coordinates to prefix coordinates
-                top++;
-                bottom++;
-                left++;
-                right++;
-
-                answer[i][j] = prefix[bottom][right]
-                        - prefix[top - 1][right]
-                        - prefix[bottom][left - 1]
-                        + prefix[top - 1][left - 1];
-            }
-        }
-
-        return answer;
-    }
+                return ans;
+   }
 }
