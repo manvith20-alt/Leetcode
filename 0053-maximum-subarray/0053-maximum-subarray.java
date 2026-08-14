208251269b320d38b6a3252f0f4c210a8c7da464
manvith20-alt <manvithnayak20@gmail.com>
2026-08-14 12:15:42 +0530

    Time: 1 ms (99.95%), Space: 77.4 MB (32.56%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cursum = nums[0];
+        int maxsum = nums[0];
+        
+        for(int i=1; i<nums.length;i++){
+            cursum+=nums[i];
+            cursum=Math.max(nums[i],cursum);
+            maxsum=Math.max(maxsum,cursum);
+        }
+        return maxsum;
+    }
+}
