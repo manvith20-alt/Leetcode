40b84d31a637c5d1457eb81ceeaae00996210de6
manvith20-alt <manvithnayak20@gmail.com>
2026-08-18 23:20:48 +0530

    Time: 47 ms (25.86%), Space: 47.3 MB (27.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,25 +1,45 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-     int n1=s.length();
-     int n2=p.length();
-     ArrayList<Integer> array = new ArrayList<>();
-     
-     int[] pcount = new int[26];
-     for(char c: p.toCharArray()){
-        pcount[c-'a']++;
-     }
+        List<Integer> res = new ArrayList<>();
 
-     for(int i=0;i<=n1-n2;i++){
-        int[] scount = new int[26];
+        if(s.length()<p.length())
+            return res;
+        HashMap<Character, Integer> pMap = new HashMap<>();
+        HashMap<Character, Integer> sMap = new HashMap<>();
 
-        for(int k=i;k<i+n2;k++){
-            scount[s.charAt(k)-'a']++;
+        for (char c : p.toCharArray()) {
+            pMap.put(c, pMap.getOrDefault(c, 0) + 1);
         }
 
-        if(Arrays.equals(scount,pcount))
-            array.add(i);
-     }
-     
-     return array; 
+        int left = 0;
+        int count = p.length();
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+
+            sMap.put(ch, sMap.getOrDefault(ch, 0) + 1);
+
+            if (pMap.containsKey(ch) && sMap.get(ch) <= pMap.get(ch)) {
+                count--;
+            }
+
+            if (right - left + 1 > p.length()) {
+                char leftChar = s.charAt(left);
+
+                if (pMap.containsKey(leftChar)
+                        && sMap.get(leftChar) <= pMap.get(leftChar)) {
+                    count++;
+                }
+
+                sMap.put(leftChar, sMap.get(leftChar) - 1);
+                left++;
+            }
+
+            if (count == 0) {
+                res.add(left);
+            }
+        }
+
+        return res;
     }
 }
